1c9d87c6d9deb234753c061239ab6dbc8eef45d7
misawa-shota <kaiyou0120@gmail.com>
2024-01-01 15:47:13 +0900

    SEO対策に関わる記述を追加#34

--- resources/views/layouts/map.blade.php
+++ resources/views/layouts/map.blade.php
@@ -7,7 +7,22 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Laravel') }} | 林道に特化したマップ検索サービス</title>
+
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('storage/favicon.ico') }}">
+    <link rel="canonical" href="{{ url()->current() }}">
+
+    <meta name="description" content="林道を検索するなら林道マップ! マップ上で林道の位置とルートが確認できるから分かりやすい。あなたの日常に冒険という選択肢を。">
+    <meta name="keywords" content="林道,冒険,自然,アドベンチャー,マップ,林道マップ">
+    <meta property="og:site_name" content="林道マップ">
+    <meta property="og:title" content="林道マップ">
+    <meta property="og:description" content="林道を検索するなら林道マップ! マップ上で林道の位置とルートが確認できるから分かりやすい。あなたの日常に冒険という選択肢を。">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="{{ url()->full() }}">
+    <meta property="og:image" content="{{ asset('storage/img/icon.svg') }}">
+    <meta property="og:locale" content="ja_JP">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@rindoumap_net">
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
